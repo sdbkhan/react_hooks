b7040ce81102bfb6b3cbc2d32ec2d2e83811e405
unknown <admin@CHETUIWK413.chetu.com>
2021-02-04 16:38:11 +0530

    file update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import react, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+const App=()=>{
+  const state= useState();
+  const[count, setCount]= useState(0);
+  const IncNum=()=>{
+    setCount(count +1);
+  };
+  return(
+    <>
+    <h1>
+      {count}
+    </h1>
+    <button onClick={IncNum}>click Me</button>
+    </>
   );
 }
-
 export default App;
